3a7f13d35cf5ba8f74e6ddf8671039957f6bb156
Trevor Danner <tjdanner01@gmail.com>
2023-03-05 17:18:30 -0600

    Fetch working as desired.

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const body = document.querySelector('body');
 const word = document.getElementById('word-input');
 const searchButton = document.getElementById('search-button');
+const wordContainer = document.createElement('div');
 
 const URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
 searchButton.onclick = e => {
+
+    wordContainer.setAttribute('id', 'word-container');
+    wordContainer.innerHTML = '';
+    console.log(word.value);
+
     fetch(`${URL}${word.value}`)
         .then(res => {
             return res.json();
         })
         .then(data => {
-            console.log(data);
-            wordContainer.innerHTML = `
-            <h1>${data[0].word}</h1>
-            <p>${data[0].phonetic}</p>
-            <p>${data[0].meanings[0].partOfSpeech}</p>
-            <p>definition</p>
-            <p>example</p>`
+            if (data.title) {
+                wordContainer.innerHTML = `<h1>That's not a word, retard.</h1>`
+            } else {
+                console.log(data);
+                wordContainer.innerHTML = `
+                <h1>${data[0].word}</h1>
+                <p>${data[0].phonetic || ''}</p>
+                <p>${data[0].meanings[0].partOfSpeech || ''}</p>
+                <p>${data[0].meanings[0].definitions[0].definition || ''}</p>
+                <p>${data[0].meanings[0].definitions[0].example || ''}</p>`
+            }
         })
-    const wordContainer = document.createElement('div');
-    wordContainer.setAttribute('id', 'word-container');
+        .catch(err => {
+            wordContainer.innerHTML = `<h1>Please enter a word.</h1>`;
+        })
 
     body.appendChild(wordContainer);
 }
