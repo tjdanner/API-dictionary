776e15a5dd5e339341289120102ae668e55115f0
Trevor Danner <tjdanner01@gmail.com>
2023-03-07 10:32:43 -0600

    Everything working except sound.

--- script.js
+++ script.js
@@ -18,11 +18,16 @@ searchButton.onclick = e => {
         })
         .then(data => {
             if (data.title) {
-                wordContainer.innerHTML = `<h1>Please enter a valid word.</h1>`
+                wordContainer.innerHTML = `<h1>Could not find word.</h1>`
             } else {
                 console.log(data);
                 wordContainer.innerHTML = `
+                <div id='word-icon-container'>
                 <h1 id='word'>${data[0].word}</h1>
+                <svg id='sound-icon' width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 11V13M6 8V16M9 10V14M12 7V17M15 4V20M18 9V15M21 11V13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                </div>
                 <div id='phonetics'>
                 <p>${data[0].phonetic || ''} • ${data[0].meanings[0].partOfSpeech || ''}</p>
                 </div>
@@ -63,4 +68,10 @@ wordInput.onfocus = e => {
     wordInput.onblur = e => {
         wordInput.style.boxShadow = '';
     }
+}
+
+const soundIcon = document.getElementById('sound-icon');
+
+soundIcon.onclick = e => {
+    container.style.backgroundColor = 'black';
 }
